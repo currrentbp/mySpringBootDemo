9f9679f31aae3562785cadb0fc16f6ea9a599605
baopan <baopan@aixuexi.com>
2020-07-26 11:39:47 +0800

    sss

---
+++ src/main/java/com/currentbp/ApplicationStarter.java
@@ -0,0 +1,18 @@
+package com.currentbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目启动点
+ *
+ * @author baopan
+ * @createTime 2020/6/16 11:51
+ */
+@SpringBootApplication
+public class ApplicationStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStarter.class, args);
+        System.out.println("sssssssssssssssss");
+    }
+}
